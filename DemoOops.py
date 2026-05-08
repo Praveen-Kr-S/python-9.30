9974db398c0577f728fa72339f1c900d50a95334
PraveenKumar S <praveenkrsslm69@gmail.com>
2026-05-08 09:40:55 +0530

    Update DemoOops.py

--- DemoOops.py
+++ DemoOops.py
@@ -78,6 +78,130 @@ c2 = Car("Hyndai","Creta","Black",1600000)
 
 
 
+#Inheritance
+#single level inheritance
+
+# class Book_info:#super class
+#     name = "Learn Python"
+#     def fun1(self):
+#         print(f"Book Name : {self.name}")
+#
+# class Author(Book_info):#sub-class
+#     au_name = "Naveen"
+#     def fun2(self):
+#         print(f"Author Name : {self.au_name}")
+
+# a = Author()
+# print(a.au_name)
+# print(a.name)
+# a.fun2()
+# a.fun1()
+
+
+
+#Multilevel Inheritance
+
+class Book_info:#super class
+    name = "Learn Python"
+    def fun1(self):
+        print(f"Book Name : {self.name}")
+
+class Author(Book_info):#sub-class
+    au_name = "Naveen"
+    def fun2(self):
+        print(f"Author Name : {self.au_name}")
+
+
+class Book_price(Author):
+    def fun3(self):
+        print("Book Price Rs : 700")
+
+class Book_genre(Book_price):
+    def fun4(self):
+        print("Book Genre : Tech Learning")
+
+# g = Book_genre()
+# g.fun4()
+# g.fun3()
+# g.fun2()
+# g.fun1()
+
+
+
+#Hierachical Inheritance
+
+class upi:
+    def UPI_money_transfer(self):
+        print("UPI Money Transfer System")
+
+class gpay(upi):
+    def gpay_access(self):
+        print("Gpay User Account Access..")
+
+class phonepy(upi):
+    def phonepy_access(self):
+        print("phonepy User Account Access..")
+
+class pytem(upi):
+    def pytem_access(self):
+        print("pytem User Account Access..")
+
+# pt = pytem()
+# pt.pytem_access()
+# pt.UPI_money_transfer()
+#
+# gp = gpay()
+# gp.gpay_access()
+# gp.UPI_money_transfer()
+
+
+
+#Multiple Inheritance
+class Camera:
+    def camera_access(self):
+        print("Camera Take Good Pictures..")
+
+class Radio:
+    def radio_access(self):
+        print("Play the Good Songs..")
+
+class Tv:
+    def tv_access(self):
+        print("Watch the Good Movies..")
+
+
+class smartphone(Camera,Radio,Tv):
+    def smartphone_access(self):
+        print("Enjoy the with friends via calling and internet..")
+
+# sm = smartphone()
+# sm.smartphone_access()
+# sm.tv_access()
+# sm.radio_access()
+# sm.camera_access()
+
+
+
+#Polymorphism
+
+#method overloading/complie time polymorphism
+class cal:
+    #default argument function
+    def add(self,a=None,b=None,c=None,d=None,e=None):
+        if a!=None and b!=None and c!=None and d!=None:
+            print("4 Arguments Add Value :",a+b+c+d)
+        elif a!=None and b!=None and c!=None:
+            print("3 Arguments Add Value :",a+b+c)
+        elif a!=None and b!=None:
+            print("2 Arguments Add Value :",a+b)
+        else:
+            print("1 Arguments Value :",a)
+
+# c = cal()
+# c.add(1,2,3,4)
+# c.add(1,2,3)
+# c.add(1,2)
+# c.add(1)
 
 
 
